e57307ee383adbfb22a7fb4f8be2e5447eddefed
victor <victor.teixeira@estudante.ifgoiano.edu.br>
2023-02-22 20:55:35 -0300

    update

---
+++ aula4/Fatura.java
@@ -0,0 +1,59 @@
+package aula4;
+
+public class Fatura {
+    String numero;
+    String descricao;
+    int quantidade;
+    double preco;
+    
+    public Fatura(String numero, String descricao, int quantidade, double preco) {
+        this.numero = numero;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getTotalFatura(){
+        double total;
+        total = preco * quantidade;
+        if (total > 0.0) {
+            return total;
+        }else{
+            total = 0.0;
+            return total;
+        }
+    }
+    
+}
---
+++ aula4/FaturaTeste.java
@@ -0,0 +1,9 @@
+package aula4;
+
+public class FaturaTeste {
+
+    public static void main(String[] args) {
+        Fatura fatura1 = new Fatura("f1", null, 5, 10.5);
+        System.out.println(fatura1.getTotalFatura());
+    }
+}

923bbc85ea370d2ffcc023947b0066dd34f7b4ff
victorteixeiraifgoiano <victor.teixeira@estudante.ifgoiano.edu.br>
2023-02-24 09:13:31 -0300

    update

---
+++ aula6/DataTeste.java
@@ -0,0 +1,9 @@
+package aula6;
+
+public class DataTeste {
+    public static void main(String[] args) {
+        Data d1 = new Data(24, 02, 2023);
+
+        d1.displayData(d1.getDia(), d1.getMes(), d1.getAno());
+    }
+}

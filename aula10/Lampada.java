c74d4276a95c402c78853a0b936d0a902db85950
victor <victor.teixeira@estudante.ifgoiano.edu.br>
2023-02-25 17:27:12 -0300

    update

---
+++ aula10/Lampada.java
@@ -0,0 +1,11 @@
+package aula10;
+
+public class Lampada {
+    boolean estado = true;
+
+    public Lampada(boolean estado) {
+        if (estado) {
+            this.estado = estado;
+        }
+    }
+}

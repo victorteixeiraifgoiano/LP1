7f8a5c2f23f2d2f8ceca98a34c8ec38d04ac8f5e
Víctor Teixeira <victor.teixeira@estudante.ifgoiano.edu.br>
2023-03-10 21:56:25 -0300

    update

---
+++ aula15/Testes.java
@@ -0,0 +1,8 @@
+package aula15;
+
+public class Testes {
+    public static void main(String[] args) {
+        Vip i1 = new Vip(0);
+        System.out.println(i1.valor);
+    }
+}
--- aula15/Vip.java
+++ aula15/Vip.java
@@ -5,6 +5,11 @@ public class Vip extends Ingresso {
     public Vip(double valor) {
         super(valor);
         // TODO Auto-generated constructor stub
+        super.valor = valor + 50;
+    }
+
+    public double retornaValor() {
+        return valor;
     }
 
 }

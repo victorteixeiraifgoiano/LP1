2dff057184987f3ddd3f2f47fad69998148ba119
Víctor Teixeira <victor.teixeira@estudante.ifgoiano.edu.br>
2023-03-10 22:04:27 -0300

    update

--- aula15/Testes.java
+++ aula15/Testes.java
@@ -3,6 +3,6 @@ package aula15;
 public class Testes {
     public static void main(String[] args) {
         Vip i1 = new Vip(0);
-        System.out.println(i1.valor);
+        System.out.println(i1.imprimeValor(0));
     }
 }

a21779589c626df9b6ba60c74e7b6abc72758800
Víctor Teixeira <victor.teixeira@estudante.ifgoiano.edu.br>
2023-03-10 21:49:12 -0300

    update

--- aula15/Ingresso.java
+++ aula15/Ingresso.java
@@ -1,6 +1,6 @@
 package aula15;
 
-// [ ] Crie uma classe chamada Ingresso que possui um valor em reais e um método imprimeValor.
+// [x] Crie uma classe chamada Ingresso que possui um valor em reais e um método imprimeValor.
 // [ ] Crie uma classe VIP, que herda Ingresso e possui um valor adicional.
 // [ ] Crie um método que retorne o valor do ingresso VIP (com o adicional incluído).
 // [ ] Crie uma classe Normal, que herda Ingresso e possui um método que imprime: "Ingresso Normal".
@@ -9,7 +9,7 @@ package aula15;
 // Esta última possui um método para retornar o valor do ingresso. Ambas as classes herdam a classe VIP.
 
 public class Ingresso {
-    double valor;
+    double valor = 50;
 
     public Ingresso(double valor) {
         this.valor = valor;

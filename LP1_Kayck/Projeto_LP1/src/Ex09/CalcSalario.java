03ee6682b9f4f8a1de1a6c5e5d59df4956a96a70
victorteixeiraifgoiano <victor.teixeira@estudante.ifgoiano.edu.br>
2023-05-20 23:00:21 -0300

    ok

---
+++ LP1_Kayck/Projeto_LP1/src/Ex09/CalcSalario.java
@@ -0,0 +1,44 @@
+package Ex09;
+
+import java.util.Scanner;
+
+public class CalcSalario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean encerrar = false;
+        do {
+            System.out.println("Código do operário:");
+            int c = sc.nextInt();
+
+            System.out.println("Número de horas trabalhadas:");
+            int n = sc.nextInt();
+
+            int e;
+            int salexcedente;
+            int sal;
+            if (n > 50) {
+                e = n - 50;
+                salexcedente = e * 20;
+                sal = 50 * 10;
+            } else {
+                e = 0;
+                salexcedente = e * 20;
+                sal = n * 10;
+            }
+
+            System.out.println("\nOperário:" + c);
+            System.out.println("Salário total = " + (sal + salexcedente));
+            System.out.println("Salário excedente = " + salexcedente);
+
+            System.out.println("\nDeseja encerrar o programa? (S/N)");
+            char condicao = sc.next().charAt(0);
+
+            while (condicao != 'S' && condicao != 'N') {
+                System.out.println("Resposta inválida.");
+                condicao = sc.next().charAt(0);
+            }
+            if (condicao == 'S') encerrar = true;
+        } while (encerrar == false);
+    }
+}
---
+++ LP1_Kayck/Projeto_LP1/src/Ex10/LeInteiro.java
@@ -0,0 +1,30 @@
+package Ex10;
+
+import java.util.Scanner;
+
+public class LeInteiro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean encerrar = false;
+        do {
+            System.out.println("Informe um número inteiro:");
+            int n = sc.nextInt();
+
+            if ((n % 2) == 0) System.out.print("O número " + n + " é par");
+            else System.out.print("O número " + n + " é ímpar");
+
+            if (n < 0) System.out.print(" e negativo.");
+            else System.out.print(" e positivo.");
+
+            System.out.println("\nDeseja encerrar o programa? (S/N)");
+            char condicao = sc.next().charAt(0);
+
+            while (condicao != 'S' && condicao != 'N') {
+                System.out.println("Resposta inválida.");
+                condicao = sc.next().charAt(0);
+            }
+            if (condicao == 'S') encerrar = true;
+        } while (encerrar == false);
+    }
+}
